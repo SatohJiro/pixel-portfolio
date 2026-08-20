79ed39401e0f8ebc82f07e38258377b7b41af38f
Anh Nguyen <trananhq2345@gmail.com>
2026-08-20 20:37:17 +0700

    refactor: remove GitHub Pages deployment configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repoName = process.env.REPO_NAME || "";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: '/pixel-portfolio',
   trailingSlash: true,
 };
 
